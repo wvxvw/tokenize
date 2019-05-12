01be2ec7825cb024c40eb36b5435c5861a213526
Nicolás Andrés Gallinal <ngallinal@hexacta.com>
2019-05-12 15:55:36 -0300

    Add a Makefile with test target. Updated CircleCI conf. Ability to run tests from within swipl repl.

--- test/test.pl
+++ test/test.pl
@@ -1,29 +1,5 @@
-#!/usr/bin/env swipl
-/** <module>  Unit tests for the tokenize library
- *
- * To run these tests, execute this file
- *
- *    ./test/test.pl
- */
-
-:- initialization(main, main).
-
-main(_Argv) :-
-    run_tests.
-
+:- use_module(prolog/tokenize).
 :- begin_tests(tokenize).
-
-:- dynamic user:file_search_path/2.
-:- multifile user:file_search_path/2.
-
-% Add the package source files relative to the current file location
-:- prolog_load_context(directory, Dir),
-   atom_concat(Dir, '/../prolog', PackageDir),
-   asserta(user:file_search_path(package, PackageDir)).
-
-:- use_module(package(tokenize)).
-
-% TESTS START HERE
 
 test('Hello, Tokenize!',
      [true(Actual == Expected)]
